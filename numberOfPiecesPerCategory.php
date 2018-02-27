7f27e0fb4e854616e994c850b9d1a25a54937ecf
Soare Bianca <bianca_ellena@yahoo.com>
2018-02-27 16:58:48 +0200

    Add files via upload

---
+++ numberOfPiecesPerCategory.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "array.php";
+
+echo "<h5>~ calculate the number of pieces per category ~</h5>";
+/**
+ * Calculati:
+ * 2. numarul de bucati pe categorii(ex. 'categ mma 10 bucati' etc);
+ */
+
+function bucatiPeCategorii($produse)
+{
+    $totalBucati = array();
+    foreach ($produse as $key=>$value) {
+        if (!isset($totalBucati[$value['categorie']] )) {
+            $totalBucati[$value['categorie']] = 0;
+        }
+        $totalBucati[$value['categorie']] += $value["bucati"];
+    }
+    return $totalBucati;
+}
+var_dump(bucatiPeCategorii($produse));
+
+$categorie = bucatiPeCategorii($produse);
+echo "<br/><b>Lista bucati/categorie:</b> $categorie[box] bucati box, $categorie[karate] bucati karate, $categorie[mma] bucati mma, $categorie[judo] bucati judo";
+/*
+foreach ($categorie as $key=>$value) {
+    $$key = $value;
+}
+echo $box;
+*/
+
+
